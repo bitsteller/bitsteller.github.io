4a8c0b46c369944cd69b3fad32cf3286ddc04dcc
Nils Breyer <nbnet@arcor.de>
2021-05-15 17:24:07 +0200

    blog?

--- site/src/.vuepress/config.js
+++ site/src/.vuepress/config.js
@@ -66,5 +66,30 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    ['@vuepress/blog', {
+        directories: [
+          {
+            id: 'post',
+            dirname: 'posts',
+            path: '/posts/',
+            pagination: {
+              perPagePosts: 2,
+            },
+          },
+        ],
+        frontmatters: [
+          {
+            id: "tag",
+            keys: ['tag', 'tags'],
+            path: '/tag/',
+            layout: 'Tag',
+            frontmatter: { title: 'Tag' },
+            itemlayout: 'Tag',
+            pagination: {
+              perPagePosts: 3
+            }
+          },
+        ]
+      }]
   ]
 }
