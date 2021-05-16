ca64ef9ada1d936f3b1cc831af248c7148b53992
Nils Breyer <nbnet@arcor.de>
2021-05-16 21:05:27 +0200

    test

---
+++ site/theme/components/IndexPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <main class="page">
+    <slot name="top" />
+
+    <!--<Content class="theme-default-content" />-->
+
+    <div>
+        <ul>
+        <li v-for="page in $pagination.pages">
+            <router-link class="page-link" :to="page.path">{{ page.title }}</router-link>
+        </li>
+        </ul>
+        <div id="pagination">
+        <router-link v-if="$pagination.hasPrev" :to="$pagination.prevLink">Prev</router-link>
+        <router-link v-if="$pagination.hasNext" :to="$pagination.nextLink">Next</router-link>
+        </div>
+    </div>
+
+    <PageEdit />
+
+    <PageNav v-bind="{ sidebarItems }" />
+
+    <slot name="bottom" />
+  </main>
+</template>
+
+<script>
+import PageEdit from '@theme/components/PageEdit.vue'
+import PageNav from '@theme/components/PageNav.vue'
+export default {
+  components: { PageEdit, PageNav },
+  props: ['sidebarItems']
+}
+</script>
+
+<style lang="stylus">
+@require '../styles/wrapper.styl'
+.page
+  padding-bottom 2rem
+  display block
+</style>
--- site/theme/layouts/Post.vue
+++ site/theme/layouts/Post.vue
@@ -1,7 +1,5 @@
 <template>
   <ParentLayout>
-    
-  {{$page.layout}}
 
   <Content/>
 
